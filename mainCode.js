733240f329218d564dbf428b2c503c5f8352b069
Blair Calderwood <blaircalderwood@live.co.uk>
2015-04-24 20:24:44 +0100

    Merged hosting menu with main menu and instructions

--- mainCode.js
+++ mainCode.js
@@ -1,5 +1,5 @@
-var tiles, player, enemy = {}, tileWidth, tileHeight, blueTurret, redTurret, scoreText, scoreTimer, serverTimer, game, buttonGroup, barBackground, barBackground2, healthBarBlue, healthBarRed;
-
+var tiles, player, enemy = {}, tileWidth, tileHeight, blueTurret, redTurret, scoreText, scoreTimer, serverTimer, game, barBackground, barBackground2, healthBarBlue, healthBarRed;
+var starterMenu, instrPage, instrButtonGroup, instrPageOpenBool, buttonGroup;
 var red = {}, blue = {};
 game = new Phaser.Game(1100, 500, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 var gridCoords = [
@@ -24,11 +24,6 @@ var tileImages = [
     {key: "neutralTile", src: "assets/pathLightTileTurnip.png"}
 ];
 
-function startGame() {
-
-
-}
-
 function preload() {
 
     game.stage.disableVisibilityChange = true;
@@ -52,22 +47,19 @@ function preload() {
 
     game.load.image('playButtonOut', 'assets/playButton.png');
     game.load.image('playButtonOver', 'assets/playButtonGo.png');
+
+    game.load.image('instructionButtonOut', 'assets/instructionButton.png');
+    game.load.image('instructionPage', 'assets/turnipInstructions.png');
+    game.load.image('backButtonOut', 'assets/backButton.png');
+    game.load.image('startMenu', 'assets/starterPage.png');
+
 }
 
 function create() {
 
     $("#hostSettings").hide();
 
-    tiles = game.add.group();
-
-    buttonGroup = game.add.group();
-
-    var button = game.make.button(game.world.centerX - 232, game.world.centerY - 96, 'playButtonOut', removeGroup, this, 2, 1, 0);
-
-    button.onInputOver.add(over, this);
-    button.onInputOut.add(out, this);
-
-    buttonGroup.add(button);
+    createButtons();
 
     //createMap();
 
@@ -80,8 +72,6 @@ function removeGroup() {
     game.world.remove(buttonGroup);
 
     buttonGroup.destroy();
-
-    $("#hostSettings").show();
 
 }
 
@@ -171,6 +161,14 @@ function update() {
         })
 
     }
+
+}
+
+function playAction() {
+
+    $("#hostSettings").show();
+    console.log('button clicked');
+    tiles = game.add.group();
 
 }
 
@@ -539,4 +537,135 @@ function getTargetTile(pointer) {
     var objY = Math.floor(pointer.y / tileHeight) * tileHeight + (tileHeight / 2);
 
     return {x: objX, y: objY};
+}
+
+function createButtons()
+{
+
+    instrPageOpenBool = false;
+
+    starterMenu = game.add.sprite(0, 0, 'startMenu');
+    buttonGroup = game.add.group();
+
+
+    var playButton = game.make.button(200, 175, 'playButtonOut', playPressed, this, 2, 1, 0);
+    playButton.width = 328;
+    playButton.height = 124;
+
+    var instructionButton = game.make.button(200, 325, 'instructionButtonOut', instrPressed, this, 2, 1, 0);
+    instructionButton.width = 328;
+    instructionButton.height = 124;
+
+    playButton.onInputOver.add(overPlay, this);
+    playButton.onInputOut.add(outPlay, this);
+
+    instructionButton.onInputOver.add(overInstr, this);
+    instructionButton.onInputOut.add(outInstr, this);
+
+    buttonGroup.add(playButton);
+    //buttonGroup.add(backButton);
+    buttonGroup.add(instructionButton);
+}
+
+
+
+
+function playPressed() {
+
+    if(instrPageOpenBool == false)
+    {
+
+        game.world.remove(buttonGroup);
+
+        buttonGroup.destroy();
+        starterMenu.destroy();
+        //instrPage.destroy();
+        //instrButtonGroup.destroy();
+
+        playAction();
+
+    }
+
+}
+
+function instrPressed() {
+
+    if(instrPageOpenBool == false)
+    {
+        instrAction();
+    }
+
+}
+
+
+function backPressed() {
+
+    backAction();
+
+}
+
+
+
+
+function overPlay() {
+    console.log('button over play');
+}
+
+function outPlay() {
+    console.log('button out play');
+}
+
+function overInstr() {
+    console.log('button over instr');
+}
+
+function outInstr() {
+    console.log('button out instr');
+}
+
+function overBack() {
+    console.log('button over back');
+}
+
+function outBack() {
+    console.log('button out back');
+}
+
+function instrAction()
+{
+    instrPageOpenBool = true;
+
+    instrPage = game.add.sprite(0, 0, 'instructionPage');
+
+    instrButtonGroup = game.add.group();
+
+    //instrPage = game.add.sprite(0, 0, 'instructionPage');
+
+    var backButton = game.make.button(game.world.width - 210, game.world.height - 64, 'backButtonOut', backPressed, this, 2, 1, 0);
+    backButton.width = 200;
+    backButton.height = 54;
+
+    backButton.onInputOver.add(overBack, this);
+    backButton.onInputOut.add(outBack, this);
+
+    instrButtonGroup.add(backButton);
+
+    instrPage.bringToTop();
+    game.world.bringToTop(instrButtonGroup);    //bring groups to top
+    //backButton.bringToTop();
+
+    // game.world.remove(starterMenu);
+
+
+
+}
+
+
+
+function backAction()
+{
+    instrPageOpenBool = false;
+    game.world.remove(instrPage);
+    game.world.sendToBack(instrButtonGroup);
+
 }
